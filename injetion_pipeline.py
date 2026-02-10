12db87998b8208715f1310b36cb1dc04599055af
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2026-02-11 01:53:57 +0500

    create vector store

--- injetion_pipeline.py
+++ injetion_pipeline.py
@@ -29,9 +29,41 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+
+def split_documents(documents, chunk_size=1000, chunk_overlap=200):
+    print(f"\nSplitting documents into chunks of {chunk_size} characters with {chunk_overlap} overlap...")
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    return chunks
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    print(f"Creating vector store in directory {persist_directory}...")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=persist_directory,
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+    print(f"Vector store created in directory {persist_directory}")
+    return vector_store
+
 def main():
     print("Starting document ingestion pipeline...")
+    #1. Lofinng documents
     documents = load_documents()
+    #2. Splitting documents into chunks
+    chunks = split_documents(documents)
+    #3. Creating vector store from chunks
+    vectorstore = create_vector_store(chunks)
+    print("Document ingestion pipeline completed successfully.")
 
 if __name__ == "__main__":
     main()
